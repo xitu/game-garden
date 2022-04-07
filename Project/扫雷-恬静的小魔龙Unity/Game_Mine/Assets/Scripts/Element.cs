1f926e48da43ec83efb88de1ca40898783815496
恬静的小魔龙 <764424567@qq.com>
2022-04-07 16:35:23 +0800

    Mine

---
+++ Project/扫雷-恬静的小魔龙Unity/Game_Mine/Assets/Scripts/Element.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Element : MonoBehaviour
+{
+    public bool mine;//判断是否是地雷
+
+    // 不同的纹理
+    public Sprite[] emptyTextures;
+    public Sprite mineTexture;
+
+    //提示信息
+    public string info;
+
+    void Start()
+    {
+        // 随机决定它是否是地雷
+        mine = Random.value < 0.15;
+
+        // 在Grid注册
+        int x = (int)transform.position.x;
+        int y = (int)transform.position.y;
+        Grid.elements[x, y] = this;
+    }
+
+    // 加载数字的纹理
+    public void loadTexture(int adjacentCount)
+    {
+        if (mine)
+            GetComponent<SpriteRenderer>().sprite = mineTexture;
+        else
+            GetComponent<SpriteRenderer>().sprite = emptyTextures[adjacentCount];
+    }
+
+    // 判断是否被点击
+    public bool isCovered()
+    {
+        //判断当前纹理的名称是不是默认值
+        return GetComponent<SpriteRenderer>().sprite.texture.name == "default";
+    }
+
+    // 鼠标点击
+    void OnMouseUpAsButton()
+    {
+        // 是雷的话
+        if (mine)
+        {
+            // 揭露所有雷
+            Grid.uncoverMines();
+
+            // 游戏结束
+            Debug.Log("Game Over");
+            info = "游戏结束";
+        }
+        else
+        {
+            // 显示相邻的数字号
+            int x = (int)transform.position.x;
+            int y = (int)transform.position.y;
+            loadTexture(Grid.adjacentMines(x, y));
+
+            // 揭露没有地雷的地区  
+            Grid.FFuncover(x, y, new bool[Grid.w, Grid.h]);
+
+            // 判断游戏是否胜利
+            if (Grid.isFinished())
+            {
+                Debug.Log("Game Win");
+                info = "游戏胜利";
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        GUIStyle fontStyle = new GUIStyle();
+        fontStyle.normal.background = null;    //设置背景填充
+        fontStyle.normal.textColor = new Color(1, 0, 0);   //设置字体颜色
+        fontStyle.fontSize = 40;       //字体大小
+        GUI.Label(new Rect(700, 200, 100, 50), info, fontStyle);
+    }
+}
---
+++ Project/扫雷-恬静的小魔龙Unity/Game_Mine/Assets/Scripts/Grid.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    // 创建一个二维数组网格
+    public static int w = 10; // 网格的长
+    public static int h = 10; // 网格的高
+    public static Element[,] elements = new Element[w, h];
+
+    // 发现所有地雷
+    public static void uncoverMines()
+    {
+        foreach (Element elem in elements)
+            if (elem.mine)
+                elem.loadTexture(0);
+    }
+
+    // 看看这个坐标上是否有地雷
+    public static bool mineAt(int x, int y)
+    {
+        // 控制坐标范围
+        if (x >= 0 && y >= 0 && x < w && y < h)
+            return elements[x, y].mine;
+        return false;
+    }
+
+    // 为一个元素计算相邻的地雷 8个方向
+    public static int adjacentMines(int x, int y)
+    {
+        //计数器
+        int count = 0;
+        if (mineAt(x, y + 1)) ++count; // 上
+        if (mineAt(x + 1, y + 1)) ++count; // 右上
+        if (mineAt(x + 1, y)) ++count; // 右
+        if (mineAt(x + 1, y - 1)) ++count; // 右下
+        if (mineAt(x, y - 1)) ++count; // 下
+        if (mineAt(x - 1, y - 1)) ++count; // 左下
+        if (mineAt(x - 1, y)) ++count; // 做
+        if (mineAt(x - 1, y + 1)) ++count; // 左上
+        //返回相邻的地雷数量
+        return count;
+    }
+
+    // 泛洪算法填充空元素
+    public static void FFuncover(int x, int y, bool[,] visited)
+    {
+        if (x >= 0 && y >= 0 && x < w && y < h)
+        {
+            // 判断是否遍历过
+            if (visited[x, y])
+                return;
+
+            // 发现元素
+            elements[x, y].loadTexture(adjacentMines(x, y));
+
+            // 发现地雷
+            if (adjacentMines(x, y) > 0)
+                return;
+
+            // 设置遍历标识
+            visited[x, y] = true;
+
+            // 递归
+            FFuncover(x - 1, y, visited);
+            FFuncover(x + 1, y, visited);
+            FFuncover(x, y - 1, visited);
+            FFuncover(x, y + 1, visited);
+        }
+    }
+
+    // 是否找到所有地雷
+
+    public static bool isFinished()
+    {
+        // 遍历数组 找到没有被地雷覆盖的元素
+        foreach (Element elem in elements)
+            if (elem.isCovered() && !elem.mine)
+                return false;
+        // 没有找到 => 全是地雷 => 游戏胜利.
+        return true;
+    }
+}
