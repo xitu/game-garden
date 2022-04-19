dd4d10570403e7e5b3a69868dee6b48970628dae
liqilong <13037658609@126.com>
2022-04-19 09:40:32 +0800

    init: ✨初始化掘金拼图

---
+++ Project/掘金拼图-季夏廿九/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<div id="admin">
+		<router-view />
+	</div>
+</template>
+<style lang="less" scoped>
+#admin {
+	position: relative;
+	width: 100vw;
+	height: 100vh;
+	// background-image: url("./assets/image/back-img.png");
+	background-attachment: fixed; // 背景图片不随页面的滚动而滚动
+	background-size: 100% 100%;
+	background-repeat: no-repeat;
+}
+</style>
